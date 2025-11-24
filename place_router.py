ec1d0c927a937344bc30ac733af6ce291899bd77
9tpie <joshuaaaaa0429@gmail.com>
2025-11-24 21:01:38 +0800

    place_router.py 程式規劃

---
+++ place_router.py
@@ -0,0 +1,23 @@
+def area_partition():
+    """
+    把面積分割成 1:1 or 1:2
+    """
+    pass
+
+def find_distance():
+    """
+    找出每一層的距離為多少
+    """
+    pass
+
+def place_the_router():
+    """
+    利用find_distance & area_partition 找到每個router的擺放位置
+    """
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
